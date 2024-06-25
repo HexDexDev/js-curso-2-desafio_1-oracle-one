c07cc4e659b3a3b08705e9e860c4c08e671a4b25
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-25 20:38:16 -0300

    initial

---
+++ app.js
@@ -0,0 +1,32 @@
+let titulo = document.querySelector("h1");
+
+titulo.innerHTML = "Hora do desafio 2";
+
+function buttonVerify() {
+
+    console.log("O botão foi clicado")
+    
+    }
+
+function alertLove() {
+
+        console.log("Eu amo JS")
+        
+        }
+
+function cityBr() {
+
+        prompt("Qual a cidade do Brasil?");
+        alert("Estive em São Paulo e lembrei de você");
+        }
+
+function bottom3() {
+
+        let numOne = prompt("digite um número");
+        let numTwo = prompt("digite outro número");
+        numberOne = parseInt(numOne);
+        numberTwo = parseInt(numTwo);
+        let sum = numberOne + numberTwo;
+        alert("A soma de " + numberOne + " e " + numberTwo + " é igual a " + sum);
+        }
+
